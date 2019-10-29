99a222b23fcf9d0728cfae75011e879013819f40
Lukas Manduch <lukas.manduch@eset.sk>
2019-10-29 16:06:09 +0100

    Create files for ci
    	new file:   make.ps1
    	new file:   requirements.txt

---
+++ make.ps1
@@ -0,0 +1,39 @@
+# Script for testing quality, managing python environment
+# and builds on CI
+
+param (
+    [Parameter(Mandatory = $false)][string] $build,
+    [switch] $all = $false,
+    [switch] $install = $false,
+    [switch] $update = $false,
+    [switch] $force = $false
+)
+
+$python_path = ""
+
+if (!$python_path) {
+    if (!$env:VIRTUAL_ENV -and !$force) {
+        Write-Error "Please activate virtualenv for python, or set python_path inside this script"
+        Exit 1
+    }
+    else {
+        $python_path = get-command python.exe -ErrorAction stop |  ForEach-Object {$_.Path}
+    }
+}
+
+if ($install) {
+    & $python_path "-m" "pip" "install" "-r" "requirements.txt"
+    if ($?) {
+        Write-Error "Installation failed"
+        Exit 1
+    }
+}
+
+if ($update) {
+    $files = Get-Content .\requirements.txt 
+    & $python_path "-m" "pip" "install" "--upgrade" pip $files
+}
+
+& $python_path "-m" "pytest"
+
+exit $?
