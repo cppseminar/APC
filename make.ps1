68b987fd7efd5cb323714c463cb6f2b4c1442edc
Peter Koscelansky <koscelansky@live.com>
2019-10-31 11:18:07 +0100

    Added pipenv to the project

--- make.ps1
+++ make.ps1
@@ -4,36 +4,44 @@
 param (
     [Parameter(Mandatory = $false)][string] $build,
     [switch] $all = $false,
-    [switch] $install = $false,
+    [switch] $install = $true,
     [switch] $update = $false,
     [switch] $force = $false,
     [switch] $test = $false,
     [string] $python_path = ""
 )
 
+$python_run = ''
 
-if (!$python_path) {
-    if (!$env:VIRTUAL_ENV -and !$force) {
-        Write-Error "Please activate virtualenv for python, or set python_path inside this script"
-        Exit 1
+if ((Get-Command "pipenv" -ErrorAction SilentlyContinue) -eq $null) {
+    if (!$python_path) {
+        if (!$env:VIRTUAL_ENV -and !$force) {
+            Write-Error "Please activate virtualenv for python, or set python_path inside this script"
+            Exit 1
+        }
+        else {
+            $python_path = get-command python.exe -ErrorAction stop |  ForEach-Object {$_.Path}
+        }
+
     }
-    else {
-        $python_path = get-command python.exe -ErrorAction stop |  ForEach-Object {$_.Path}
-    }
+    $python_run = '"' + $python_path + '" -m '
 }
 
+$command = $python_run
+
 if ($install) {
-    & $python_path "-m" "pip" "install" "-r" "requirements.txt"
+    $command += "pipenv install"
 }
 
 if ($update) {
-    $files = Get-Content .\requirements.txt 
-    & $python_path "-m" "pip" "install" "--upgrade" pip $files
+    & $python_run "pipenv" "update"
 }
 
 if ($test) {
-    & $python_path "-m" "pytest"
+    & $python_run "pytest"
 }
+
+Invoke-Expression "& $command"
 
 if ($?) {
     Exit 0
