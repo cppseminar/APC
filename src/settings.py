6e39cc841d041059fe1a4f0b543bd6e17c7c7f16
Peter Koscelanský <koscelansky@users.noreply.github.com>
2019-10-31 16:10:18 +0100

    Add proper settings (#4)
    * Added proper settings
    
    * fixup! Added proper settings

--- src/settings.py
+++ src/settings.py
@@ -2,11 +2,35 @@
 able to parse arguments, and later config files.  Basically you will just ask
 this module for any setting you like"""
 
+import argparse
 
 SETTINGS = None
 
 class Settings:
     def __init__(self):
-        self.verbose = False
+        parser = argparse.ArgumentParser(description="Compile list of files and run tests")
+        parser.add_argument("-d", "--dir",
+                            help="if used it is expected to use path to directory instead of files",
+                            action="store_true")
+        parser.add_argument('--test', help="path to the json file with test cases")
+        parser.add_argument('--test-data', help="path to folder with test data", default='.')
+        parser.add_argument('--output', help="path to otput directory")
+        parser.add_argument('--verbose')
+        parser.add_argument('files', nargs='+')
+        args = parser.parse_args()
+
+        self.files = args.files
+        self.test_data = args.test_data
+        self.test = args.test
+        self.dir = args.dir
+        self.output = self.get_output_dir(args.output)
+        self.verbose = args.verbose
+    
+    def get_output_dir(self, output):
+        if output is None:
+            import tempfile
+            return tempfile.mkdtemp(prefix='students')
+        else:
+            return output
 
 SETTINGS = Settings()
