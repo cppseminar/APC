493b8d19d9d1c13b14cb5f63384e4239309e9b42
Lukas <manduch.lukas@gmail.com>
2020-08-19 00:00:46 +0200

    Bugfix: task endpoint shows task if you have permission
    If you requested concrete task and you were not admin, our app logic
    decided, that you don't have permission for this.

--- functions/tasks/__init__.py
+++ functions/tasks/__init__.py
@@ -1,3 +1,4 @@
+"""Endpoint for getting and listing tasks."""
 import azure.functions as func
 
 from ..shared import http, decorators, mongo, users, core
@@ -10,6 +11,7 @@ ROUTE_PARAMS = {
     }
 }
 
+
 def _user_validator(value):
     if core.is_email(value):
         return str(value)
@@ -17,15 +19,24 @@ def _user_validator(value):
 
 
 QUERY_PARAMS = {
- "user": {
+    "user": {
         decorators.VALIDATOR: _user_validator,
         decorators.DESTINATION: "user_filter",
     }
 }
 
-@decorators.validate_parameters(route_settings=ROUTE_PARAMS, query_settings=QUERY_PARAMS)
+
+@decorators.validate_parameters(
+    route_settings=ROUTE_PARAMS, query_settings=QUERY_PARAMS
+)
 @decorators.login_required
 def get_handler(req, user: users.User, submission_id=None, user_filter=None):
+    """Handler for HTTP GET."""
+    if submission_id:
+        # User wants to see concrete task
+        roles = None if user.is_admin else user.roles
+        return get_task(submission_id, roles)
+
     if user_filter and user_filter != user.email:
         # We suppport query only for our own email
         return http.response_forbidden()
@@ -33,19 +44,24 @@ def get_handler(req, user: users.User, submission_id=None, user_filter=None):
         # Only admin can query all
         return http.response_forbidden()
     roles = None
-    if user_filter:
+    if user_filter:  # If this is not set, user is admin. We checked that
         roles = user.roles
     # AUTH is done
+    result = mongo.MongoTasks.get_tasks(roles=roles)
+    return http.response_ok(list(result))
 
-    # Get all tasks
-    if not submission_id:
-        result = mongo.MongoTasks.get_tasks(roles=roles)
-        return http.response_ok(list(result))
-    # Get concrete task
-    result = mongo.MongoTasks.get_task(submission_id, roles=roles)
+
+def get_task(task_id: str, roles):
+    """Get concrete task, based on task id.
+
+    If roles doesn't match those of task, return 404
+    """
+    result = mongo.MongoTasks.get_task(task_id, roles=roles)
     if not result:
         return http.response_not_found()
     return http.response_ok(result)
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    """Entry point."""
     return http.dispatcher(get=get_handler)(req)
