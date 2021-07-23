7f0a1b333cfb094f96d77637c5bb61fbf4e3e778
Lukas Manduch <manduch.lukas@gmail.com>
2021-07-23 19:41:13 +0200

    Add razor pages to presentation

--- cppseminar/presentation/Startup.cs
+++ cppseminar/presentation/Startup.cs
@@ -16,6 +16,12 @@ namespace presentation
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
+            services.Configure<Microsoft.AspNetCore.Routing.RouteOptions>(options =>
+            {
+                options.AppendTrailingSlash = true;
+                options.LowercaseUrls = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +36,11 @@ namespace presentation
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello razor!");
+                //});
+                endpoints.MapRazorPages();
             });
         }
     }
