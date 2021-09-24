b890efebee9cb12fa1b6b9f3da5f56d93c3decf9
Peter Koscelansky <koscelansky@eset.sk>
2021-09-24 19:15:42 +0200

    Deploy is not working

--- services/scripts/deploy.sh
+++ services/scripts/deploy.sh
@@ -9,5 +9,5 @@ apt-get -y install subversion
 svn checkout https://github.com/cppseminar/APC/trunk/services ./queued
 
 # call install, parameters are docker credentials (username, password)
-chmod +x ~/queued/scripts/install.sh
-( cd ~/queued/scripts && exec ./install.sh $1 $2 $3 )
+chmod +x ./queued/scripts/install.sh
+( cd ./queued/scripts && exec ./install.sh $1 $2 $3 )
