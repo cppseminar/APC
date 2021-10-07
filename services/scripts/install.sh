fbca2350306e2ae8e8d54b347c0a2046476e6867
Peter Koscelansky <koscelansky@eset.sk>
2021-10-07 16:18:55 +0200

    Queued: update install.sh, to download docker properly

--- services/scripts/install.sh
+++ services/scripts/install.sh
@@ -27,8 +27,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o
 
 # use stable repository
 echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
 
 # install docker
 apt-get -y install docker-ce docker-ce-cli containerd.io
