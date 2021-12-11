451c2cfd11919e45e17b198b2d4e61463d9d67ba
Lukas Manduch <manduch.lukas@gmail.com>
2021-12-11 14:15:17 +0100

    Test service: Add healthcheck failure warnings

--- cppseminar/testservice/Services/HealthCheckService.cs
+++ cppseminar/testservice/Services/HealthCheckService.cs
@@ -4,15 +4,18 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
 
 namespace testservice.Services
 {
     public class RabbitMQHealthCheck : IHealthCheck
     {
+        private ILogger<RabbitMQHealthCheck> _logger;
         private RabbitMQService _rabbitMQService;
 
-        public RabbitMQHealthCheck(RabbitMQService rabbitMQService)
+        public RabbitMQHealthCheck(RabbitMQService rabbitMQService, ILogger<RabbitMQHealthCheck> logger)
         {
+            _logger = logger;
             _rabbitMQService = rabbitMQService;
         }
 
@@ -22,6 +25,7 @@ namespace testservice.Services
         {
             if (!_rabbitMQService.HealthCheck())
             {
+                _logger.LogWarning("Healthcheck on rabbitmq failed");
                 return Task.FromResult(HealthCheckResult.Unhealthy("Rabbit mq not ready"));
             }
             return Task.FromResult(HealthCheckResult.Healthy("ok"));
@@ -31,12 +35,14 @@ namespace testservice.Services
     public class CosmosHealthCheck : IHealthCheck
     {
         private Container _container;
+        private ILogger<CosmosHealthCheck> _logger;
 
-        public CosmosHealthCheck(IConfiguration config)
+        public CosmosHealthCheck(IConfiguration config, ILogger<CosmosHealthCheck> logger)
         {
             _container = new CosmosClient(config["COSMOS_CONNECTION_STRING"])
                 .GetDatabase(config["COSMOS_DB_NAME"])
                 .GetContainer(DbConstants.TestCaseCollection);
+            _logger = logger;
         }
 
         public async Task<HealthCheckResult> CheckHealthAsync(
@@ -48,6 +54,7 @@ namespace testservice.Services
                 var result = await _container.ReadContainerAsync();
                 if ((int)result.StatusCode > 299)
                 {
+                    _logger.LogWarning("Healthcheck on cosmosDb failed with {code}", result.StatusCode);
                     return HealthCheckResult.Unhealthy($"Cannot read cosmos db, code {result.StatusCode}");
                 }
                 return HealthCheckResult.Healthy("ok");
