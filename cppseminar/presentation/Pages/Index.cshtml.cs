c87a837a24d902d8a37d60a36eef0a343a169cbe
Peter Koscelansky <koscelansky@live.com>
2022-10-10 09:14:58 +0200

    Get project link from env

--- cppseminar/presentation/Pages/Index.cshtml.cs
+++ cppseminar/presentation/Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace presentation.Pages
 {
     [AllowAnonymous]
     public class IndexModel : PageModel
     {
+        public string DocsLink = Environment.GetEnvironmentVariable("LINKS_CURRENT_DOCS");
     }
 }
