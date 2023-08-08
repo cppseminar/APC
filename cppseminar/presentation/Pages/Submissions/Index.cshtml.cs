82fabbbd9b4d1bbdf15fa349b347e3c4b6a17ff1
to-0 <tomas1.kalny@gmail.com>
2023-08-08 14:44:28 +0200

    First version of paging

--- cppseminar/presentation/Pages/Submissions/Index.cshtml.cs
+++ cppseminar/presentation/Pages/Submissions/Index.cshtml.cs
@@ -20,23 +20,34 @@ namespace presentation.Pages.Submissions
             _submissionService = submissionService;
         }
 
-        public async Task OnGetAsync(int? page)
+        public async Task OnGetAsync()
         {
-            try
-            {
-                Submissions = await _submissionService.GetUserSubmissionsAsync(User.GetEmail());
+            System.Console.WriteLine("pomoc");
+            System.Console.WriteLine(PageNumber);
+            if (!Submissions.Any()){
+                try
+                {
+                    Submissions = await _submissionService.GetUserSubmissionsAsync(User.GetEmail());
+                    numberOfPages = (int)Math.Ceiling(Convert.ToDouble(Submissions.Count()) / Convert.ToDouble(pageSize));
+                }
+                catch(OperationFailedException e)
+                {
+                    ModelState.AddModelError(string.Empty, e.Message);
+                }
             }
-            catch(OperationFailedException e)
-            {
-                ModelState.AddModelError(string.Empty, e.Message);
-            }
-            
+            int startIndex = PageNumber*pageSize;
+            TakenSubmissions = Submissions.Skip(startIndex).Take(pageSize);
         }
 
         [BindProperty]
         public Submission MySubmission { get; set; }
         public IList<string> Errors = new List<string>();
         public IEnumerable<Submission> Submissions = Enumerable.Empty<Submission>();
+        public IEnumerable<Submission> TakenSubmissions = Enumerable.Empty<Submission>();
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; }
+        public int pageSize = 10;
+        public int numberOfPages = 0;
 
         private ILogger<IndexModel> _logger = null;
         private SubmissionService _submissionService = null;
