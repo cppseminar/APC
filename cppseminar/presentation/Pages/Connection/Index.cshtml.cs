2066ac56d419c23f6437abe3b384c83f0cf45fc0
Adam Berenik <adam.berenik@eset.com>
2023-08-17 16:22:14 +0200

    presentation modified to serve static files in /wwwroot folder + added dummy Connection page

---
+++ cppseminar/presentation/Pages/Connection/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using presentation.Model;
+using presentation.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace presentation.Pages.Connection
+{
+    public class IndexModel : PageModel
+    {
+        private ILogger<IndexModel> _logger;
+        public bool IsAdmin = false;
+    }
+}
--- cppseminar/presentation/Startup.cs
+++ cppseminar/presentation/Startup.cs
@@ -88,11 +88,14 @@ namespace presentation
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            System.Console.WriteLine(env.ContentRootPath);
             app.UseForwardedHeaders();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            app.UseStaticFiles();
 
             app.UseRouting();
             app.UseAuthentication();
