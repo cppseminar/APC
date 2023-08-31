f94bb54844c0e27e1a2ba48a6d43ef7dd1579be8
Adam Berenik <adam.berenik@eset.com>
2023-08-31 09:28:59 +0200

    IP filter now supports range of IP addresses

--- cppseminar/presentation/Filters/IPFilter.cs
+++ cppseminar/presentation/Filters/IPFilter.cs
@@ -1,22 +1,53 @@
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Net;
 namespace presentation.Filters;
+
 public class TestIPFilter : ResultFilterAttribute
 {
-    private readonly IPAddress _allowedIPAddress;
+    private readonly byte[] _allowedLowerBytes;
+    private readonly byte[] _allowedUpperBytes;
 
-    public TestIPFilter(string allowedIPAddress)
+    public TestIPFilter(string allowedIPLowerStr, string allowedIPUpperStr)
     {
-        IPAddress.TryParse(allowedIPAddress, out _allowedIPAddress);
+        IPAddress allowedIPLower;
+        IPAddress.TryParse(allowedIPLowerStr, out allowedIPLower);
+        _allowedLowerBytes = allowedIPLower.GetAddressBytes();
+
+        IPAddress allowedIPUpper;
+        IPAddress.TryParse(allowedIPUpperStr, out allowedIPUpper);
+        _allowedUpperBytes = allowedIPUpper.GetAddressBytes();
+
+        // Check if lower bound is indeed lower
+        for (int i = 0; i < _allowedLowerBytes.Length; i++)
+        {
+            if (_allowedLowerBytes[i] > _allowedUpperBytes[i])
+            {
+                throw new ArgumentException("Invalid range of IP addresses.");
+            }
+        }
+    }
+
+    private bool AddressWithinRange(IPAddress clientAddress)
+    {
+        byte[] clientAddressBytes = clientAddress.GetAddressBytes();
+        for (int i = 0; i < _allowedLowerBytes.Length; i++)
+        {
+            if (clientAddressBytes[i] < _allowedLowerBytes[i] || clientAddressBytes[i] > _allowedUpperBytes[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public override void OnResultExecuting(ResultExecutingContext context)
     {
-        System.Console.WriteLine("On result executing is here");
-        string remoteIpAddress = context.HttpContext.Connection.RemoteIpAddress?.ToString();
+        IPAddress clientIPAddress;
+        IPAddress.TryParse(context.HttpContext.Connection.RemoteIpAddress?.ToString(), out clientIPAddress);
 
-        if (remoteIpAddress != _allowedIPAddress?.ToString())
+        if (!AddressWithinRange(clientIPAddress))
         {
             context.Result = new ContentResult
             {
--- cppseminar/presentation/Pages/Connection/Index.cshtml.cs
+++ cppseminar/presentation/Pages/Connection/Index.cshtml.cs
@@ -9,15 +9,20 @@ using presentation.Filters;
 
 namespace presentation.Pages.Connection
 {
-    [TestIPFilter("178.41.87.254")]
+    [TestIPFilter("172.18.0.0", "172.18.255.255")]
     public class IndexModel : PageModel
     {
         private ILogger<IndexModel> _logger;
-        public bool IsAdmin = false;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task OnGetAsync(){
-            System.Console.WriteLine(Request.Headers["X-Forwarded-For"]); // this should be able to extract the original IP adress, after it goes through kubernetes
-            var clientIPAdress = Request.HttpContext.Connection.RemoteIpAddress.ToString(); // extracting ip adress locally
-            System.Console.WriteLine(clientIPAdress);
+            _logger.LogInformation(Request.Headers["X-Forwarded-For"]); // this should be able to extract the original IP adress, after it goes through kubernetes
+            var clientIPAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString(); // extracting ip adress locally
+            _logger.LogInformation($"Client IP address: {clientIPAddress}");
         }
     }
     
